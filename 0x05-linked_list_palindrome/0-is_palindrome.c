d3ee18c86c231c184a0eaba40ae3f4abf978f353
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-05-27 01:41:16 -0500

    Added the project 0x05-linked_list_palindrome - Added a function to check if a singly linked list is a palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,100 @@
+#include "lists.h"
+
+/**
+ * listint_len - Computes the number of elements in a linked listint_t list.
+ *
+ * @h: the const pointer to the list (it means the HEAD node).
+ *
+ * Return: the number of nodes(elements) of the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	const listint_t *iterator;
+
+	count = 0;
+	iterator = h;
+
+	while (iterator)
+	{
+		count++;
+		iterator = iterator->next;
+	}
+
+	return (count);
+}
+
+/**
+ * get_nodeint_at_index - Find the nth node of a listint_t linked list.
+ *
+ * @head: a pointer to the list
+ *		  it means a pointer to the HEAD node.
+ *
+ * @index: the integer that identifies the position of the node in the list.
+ *
+ * Return: the nth node of a listint_t list or NULL if the node does not exist.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *iterator;
+	unsigned int i;
+
+	iterator = head;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (iterator->next == NULL)
+		{
+			return (NULL);
+		}
+
+		iterator = iterator->next;
+	}
+
+	return (iterator);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ *
+ * @head: the pointer to the starpoint of the list (it means the HEAD node).
+ *
+ * Return:	0 if it is not a palindrome, 1 if it is a palindrome, also
+ *			an empty list is considered a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int left, right, end;
+	listint_t *iterator;
+
+	if (head == NULL)
+		return (1);
+
+	iterator = *head;
+
+	if (iterator == NULL)
+		return (1);
+
+	end = listint_len(iterator) - 1;
+
+	while (end > 0)
+	{
+		left = iterator->n;
+		right = get_nodeint_at_index(iterator, end)->n;
+
+		if (left != right)
+			return (0);
+
+		iterator = iterator->next;
+		end -= 2;
+	}
+
+	return (1);
+}
---
+++ 0x05-linked_list_palindrome/README.md
@@ -0,0 +1,26 @@
+# 0x05. Linked list palindrome
+
+---
+
+## Learning Objectives
+
+The objective of this project is implement an algorithm to check if a single linked list is a
+palindrome in an optimum way, It means solving it with a technical interview perspective.
+
+---
+
+### [0. Linked list palindrome](./0-is_palindrome.c)
+
+A function in C that checks if a singly linked list is a palindrome.
+
+```C
+* Prototype: int is_palindrome(listint_t **head);
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+* An empty list is considered a palindrome
+```
+
+---
+
+## Author
+
+* **Nicolas Forero Puello** - [NickForero11](https://github.com/NickForero11)
