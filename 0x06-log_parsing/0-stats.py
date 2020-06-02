2947a2385d15e850a67620fec56c3d1e0f991443
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-06-01 23:42:03 -0500

    Remove unused import and refactorized

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module to compute metrics from logs.
 """
-from sys import (stdin, stdout)
+from sys import stdin
 
 total_size = 0
 
@@ -15,22 +15,28 @@ stats = {
 lines = 0
 
 try:
+
     for line in stdin:
+
         data = line.split(' ')
+
         if len(data) > 6:
             status, size = data[-2:]
             total_size += int(size)
             if status in stats:
                 stats[status] += 1
             lines += 1
+
         if lines == 10:
             print('File size: {}'.format(total_size))
             for (code, count) in sorted(stats.items()):
                 if count > 0:
                     print('{}: {}'.format(code, count))
                 lines = 0
+
 except KeyboardInterrupt:
     pass
+
 finally:
     print('File size: {}'.format(total_size))
     for (code, count) in sorted(stats.items()):
