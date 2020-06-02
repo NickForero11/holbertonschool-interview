ea5922e47f213453a7189da27282d274845356ab
Nicolas Forero Puello <nicolasforero1199@gmail.com>
2020-06-01 23:33:41 -0500

    Modified the printing order and added extra checking of the input data

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Module to compute metrics from logs.
+"""
 from sys import (stdin, stdout)
 
 total_size = 0
 
-statuses = {
+stats = {
     "200": 0,
     "301": 0,
     "400": 0, "401": 0, "403": 0, "404": 0, "405": 0,
@@ -14,13 +16,16 @@ lines = 0
 
 try:
     for line in stdin:
-        status, size = line.split(' ')[7:]
-        total_size += int(size)
-        statuses[status] += 1
-        lines += 1
+        data = line.split(' ')
+        if len(data) > 6:
+            status, size = data[-2:]
+            total_size += int(size)
+            if status in stats:
+                stats[status] += 1
+            lines += 1
         if lines == 10:
             print('File size: {}'.format(total_size))
-            for (code, count) in sorted(statuses.items()):
+            for (code, count) in sorted(stats.items()):
                 if count > 0:
                     print('{}: {}'.format(code, count))
                 lines = 0
@@ -28,6 +33,6 @@ except KeyboardInterrupt:
     pass
 finally:
     print('File size: {}'.format(total_size))
-    for (code, count) in sorted(statuses.items()):
+    for (code, count) in sorted(stats.items()):
         if count > 0:
             print('{}: {}'.format(code, count))
